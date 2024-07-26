0aab56cc414048b73af07acfadb4c42eac5d4acc
arztareef <thetareef4rz@skiff.com>
2024-07-27 04:45:41 +0600

    Fixed package amount not showing and added icons to the fetch and aligned information to the center
    - fixed package not showing
    - added icons
    - aligned the information in vertical center direction to the PolarisOS
    Icon

--- polarfetch.sh
+++ polarfetch.sh
@@ -22,7 +22,8 @@ CPU="$(lscpu | grep Model\ name | sed 's/name:\ //' | sed "s/([^)]*)/()/g" | sed
 GPU="$(glxinfo -B | grep Device: | sed "s/([^)]*)/()/g" | sed 's/[)(]//g')"
 RAM="$(free -h | awk '/^Mem:/ {print $3 " / " $2}')"
 OS1="$(lsb_release -si)"
-OS="$(lsb_release -sirc)"
+Packages="$(pacman -Q | wc -l)"
+# OS="$(lsb_release -sirc)" -- NOTE: ALWAYS SHOWS ROLLING
 
 if [ -z "${WM}" ]; then
 	if [ "${XDG_CURRENT_DESKTOP}" ]; then
@@ -41,30 +42,23 @@ else
 fi
 
 # colors
- c1="\e[31m" # Red
- c2="\e[32m" # Light Green
- c3="\e[33m" # Yellow
- c4="\e[34m"  # Calm Blue
- c5="\e[35m" # Violet
- c6="\e[36m" # Arch Blue
- c7="\e[37m" # White
- c8="\e[38m" # Sky Blue
- c9="\e[39m" # Light Grey
- c10="\e[30m" # Dark Grey
- c20="\e[0m" # Normal
+c1="\e[31m" # Red
+c2="\e[32m" # Light Green
+c3="\e[33m" # Yellow
+c4="\e[34m"  # Calm Blue
+c5="\e[35m" # Violet
+c6="\e[36m" # Arch Blue
+c7="\e[37m" # White
+c8="\e[38m" # Sky Blue
+c9="\e[39m" # Light Grey
+c10="\e[30m" # Dark Grey
+c20="\e[0m" # Normal
 
 # Package count for Debian based systems
-Packages_dpkg(){
-         
-         Packages="$(dpkg -l | grep -c ^i)"
-}
-
-# Package count for Arch based systems
-Arch_Packages(){
-
-         Packages="$(pacman -Qs | wc -l)"
-
-}
+# Packages_dpkg(){
+#
+#     Packages="$(dpkg -l | grep -c ^i)"
+# }
 
 # An Error echo wrapper
 err0r()
@@ -78,29 +72,32 @@ err0r()
 # A Warning echo wrapper
 warn()
 {
- echo >&2 "$(tput bold; tput setaf 3)[!] WARNING: ${*}$(tput sgr0)"
- echo ""
+    echo >&2 "$(tput bold; tput setaf 3)[!] WARNING: ${*}$(tput sgr0)"
+    echo ""
 }
 
 # A Message echo wrapper
 msg()
 {
- echo "$(tput bold; tput setaf 2)[+] ${*}$(tput sgr0)"
+    echo "$(tput bold; tput setaf 2)[+] ${*}$(tput sgr0)"
 }
 
  
 echo ""
-echo -e "${c5}              &&&&              "     ${user}${c20}$(hostname)                                                          
-echo -e "${c5}           &&&@@@@&&&           "     OS: ${c7}$OS 
-echo -e "${c5}         &&&@@&&&&@@&&&         "     $envtype: ${c7}$WM 
-echo -e "${c5}       &&&@@&&&&&&&&@@&&&       "     Kernel: ${c7}$Kernel
-echo -e "${c5}     &&&@@&#&&@@@@&&#&@@&&&     "     Packages: ${c7}$Packages
-echo -e "${c5}   &&&@@&#&&@@BBBB@@&&#&@@&&&   "     Shell: ${c7}$Shell
-echo -e "${c5} #&@@@&#&@@@BG&&&&GB@@@&#&@@@&# "     CPU: ${c7}$CPU
-echo -e "${c5}  &@@@&#&@@@BG&&&&GB@@@&#&@@@&# "     GPU: ${c7}$GPU
-echo -e "${c5}   &&&@@&#&&@@BBBB@@&&#&@@&&&   "     RAM: ${c2}$RAM
-echo -e "${c5}     &&&@@&#&&@@@@&&#&@@&&&     "     Uptime:  ${c20}$Uptime
-echo -e "${c5}       &&&@@&&&&&&&&@@&&&       "
-echo -e "${c5}         &&&@@&&&&@@&&&         "
-echo -e "${c5}           &&&@@@@&&&           "
-echo -e "${c5}              &&&&              "
+echo -e "${c5}              &&&&                   "     
+echo -e "${c5}           &&&@@@@&&&                "          " "${user}${c20}$(hostname)
+echo -e "${c5}         &&&@@&&&&@@&&&              "         --------------------------------------                    
+echo -e "${c5}       &&&@@&&&&&&&&@@&&&            "         󰍹 "  "OS: ${c7}$OS1
+echo -e "${c5}     &&&@@&#&&@@@@&&#&@@&&&          "          "  "$envtype: ${c7}$WM 
+echo -e "${c5}   &&&@@&#&&@@BBBB@@&&#&@@&&&        "         󰻀"   "Kernel: ${c7}$Kernel
+echo -e "${c5} #&@@@&#&@@@BG&&&&GB@@@&#&@@@&#      "          "  "Packages: ${c7}$Packages
+echo -e "${c5}  &@@@&#&@@@BG&&&&GB@@@&#&@@@&#      "          "  "Shell: ${c7}$Shell
+echo -e "${c5}   &&&@@&#&&@@BBBB@@&&#&@@&&&        "          "  "CPU: ${c7}$CPU
+echo -e "${c5}     &&&@@&#&&@@@@&&#&@@&&&          "         󰻏 "  "GPU: ${c7}$GPU
+echo -e "${c5}       &&&@@&&&&&&&&@@&&&            "          "  "RAM: ${c2}$RAM
+echo -e "${c5}         &&&@@&&&&@@&&&              "          "  "Uptime:  ${c20}$Uptime
+echo -e "${c5}           &&&@@@@&&&                "
+echo -e "${c5}              &&&&                   "
+echo ""
+echo ""
+echo ""
